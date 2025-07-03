3764c9d24456df7f6460d999b6da03a7bb56a5bf
Ayesha Nizam <ayeshanizam39@gmail.com>
2025-07-03 18:02:06 +0500

    commit

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -158,7 +158,7 @@ const Projects= () => {
                 <div
                   key={t.id}
                    onClick={() => setSelectedIndex(index)}
-                  className={`cursor-pointer bg-white w-full md:w-[500px] rounded-lg shadow-md p-4 border-2 border-purple-300 transform hover:scale-105 transition duration-300 ease-in-out animate-fadeIn flex items-start ${
+                  className={`cursor-pointer bg-white w-full rounded-lg shadow-md p-4 border-2 border-purple-300 transform hover:scale-105 transition duration-300 ease-in-out animate-fadeIn flex items-start ${
                 index === selectedIndex
                 ? "bg-purple-100 border-purple-400"
                 : "bg-white border-gray-300"
@@ -188,7 +188,7 @@ const Projects= () => {
                 className="absolute top-2 right-2 text-purple-600 text-sm hover:text-red-700"
                 title="Delete testimonial"
               >
-                ❌
+                🗑
               </button>
                     </div>
                   </div>
